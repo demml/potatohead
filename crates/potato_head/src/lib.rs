8a110259d727233ddfefde50c298186695290bd2
Thorrester <sjforrester32@gmail.com>
2025-06-30 19:09:04 -0400

    Update lib.rs

--- crates/potato_head/src/lib.rs
+++ crates/potato_head/src/lib.rs
@@ -2,7 +2,7 @@ pub use potato_agent::*;
 pub use potato_prompt::*;
 pub use potato_type::*;
 pub use potato_util::{
-    create_uuid7, json_to_pyobject, json_to_pyobject_value, pyobject_to_json, version,
-    PyHelperFuncs,
+    create_uuid7, error::UtilError, json_to_pyobject, json_to_pyobject_value, pyobject_to_json,
+    version, PyHelperFuncs,
 };
 pub use potato_workflow::*;
