615093329b0a2ba9fe4606b0336a009fd09aee80
Thorrester <sjforrester32@gmail.com>
2025-07-02 13:52:34 -0400

    Update lib.rs

--- crates/potato_prompt/src/lib.rs
+++ crates/potato_prompt/src/lib.rs
@@ -3,5 +3,5 @@ pub mod prompt;
 pub use prompt::{
     error::PromptError,
     interface::{ModelSettings, Prompt},
-    types::{AudioUrl, BinaryContent, DocumentUrl, ImageUrl, Message, PromptContent, Role},
+    types::{AudioUrl, BinaryContent, DocumentUrl, ImageUrl, Message, PromptContent, Role, Score},
 };
