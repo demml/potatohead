7f1a4e63db8ee83aa9e7dd1a49c72e1805101ba7
Thorrester <sjforrester32@gmail.com>
2025-07-21 16:19:51 -0400

    expose weighted calc

--- crates/potato_head/src/lib.rs
+++ crates/potato_head/src/lib.rs
@@ -1,9 +1,9 @@
 pub use potato_agent::*;
 pub use potato_prompt::*;
-pub use potato_type::{c, Provider, SaveName, StructuredOutput};
+pub use potato_type::{Provider, SaveName, StructuredOutput};
 pub use potato_util::{
-    create_uuid7, error::UtilError, json_to_pydict, json_to_pyobject, pyobject_to_json, version,
-    PyHelperFuncs,
+    calculate_weighted_score, create_uuid7, error::UtilError, json_to_pydict, json_to_pyobject,
+    pyobject_to_json, version, PyHelperFuncs,
 };
 
 pub use potato_workflow::*;
--- crates/potato_util/src/lib.rs
+++ crates/potato_util/src/lib.rs
@@ -3,5 +3,6 @@ pub mod utils;
 
 pub use error::UtilError;
 pub use utils::{
-    create_uuid7, json_to_pydict, json_to_pyobject, pyobject_to_json, version, PyHelperFuncs,
+    calculate_weighted_score, create_uuid7, json_to_pydict, json_to_pyobject, pyobject_to_json,
+    version, PyHelperFuncs,
 };
