c7fe214de1db69f01e3c162b70ad0359bac66838
Thorrester <sjforrester32@gmail.com>
2025-08-06 09:42:30 -0400

    lints

--- py-potato/tests/test_workflow.py
+++ py-potato/tests/test_workflow.py
@@ -145,9 +145,7 @@ def test_potato_head_workflow():
         open_agent1 = Agent(Provider.OpenAI)
         open_agent2 = Agent(Provider.OpenAI)
 
-        workflow = Workflow(
-            name="test_workflow"
-        )  # expand named argument to allow agents and tasks
+        workflow = Workflow(name="test_workflow")  # expand named argument to allow agents and tasks
         workflow.add_agent(open_agent1)  # allow adding list of agents
         workflow.add_agent(open_agent2)
         workflow.add_task(  # allow adding list of tasks
@@ -226,9 +224,7 @@ def test_potato_head_workflow_structured_output():
         open_agent1 = Agent(Provider.OpenAI)
         open_agent2 = Agent(Provider.OpenAI)
 
-        workflow = Workflow(
-            name="test_workflow"
-        )  # expand named argument to allow agents and tasks
+        workflow = Workflow(name="test_workflow")  # expand named argument to allow agents and tasks
         workflow.add_agent(open_agent1)  # allow adding list of agents
         workflow.add_agent(open_agent2)
         workflow.add_task(  # allow adding list of tasks
@@ -362,9 +358,7 @@ def test_workflow_param_binding():
         )
 
         agent = Agent(Provider.OpenAI)
-        workflow = Workflow(
-            name="test_workflow"
-        )  # expand named argument to allow agents and tasks
+        workflow = Workflow(name="test_workflow")  # expand named argument to allow agents and tasks
         workflow.add_agent(agent)
         workflow.add_task(
             Task(
