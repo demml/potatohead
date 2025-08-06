938c04f8d7a3f6fec8b0a5c7218a7c096df04ae9
Thorrester <sjforrester32@gmail.com>
2025-08-06 10:21:29 -0400

    add initial docs and example

---
+++ py-potato/examples/potato_joke.py
@@ -0,0 +1,23 @@
+# type: ignore
+from potato_head import Prompt, Agent, Provider
+from potato_head.logging import RustyLogger, LoggingConfig, LogLevel
+
+RustyLogger.setup_logging(LoggingConfig(log_level=LogLevel.Debug))
+
+
+prompt = Prompt(
+    model="o4-mini",
+    provider="openai",
+    message="Tell me a joke about potatoes.",
+    system_instruction="You are a helpful assistant.",
+)
+
+if __name__ == "__main__":
+    # This is just to ensure the script can be run directly
+    print("Running potato joke example...")
+    agent = Agent(Provider.OpenAI)
+    response = agent.execute_prompt(prompt=prompt)
+
+    print(response.result)
+    # Why did the potato win the talent show?
+    # Because it was outstanding in its field!
