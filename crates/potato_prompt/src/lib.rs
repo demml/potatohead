b6d8bef326a8226e17ab7605ddc0548c870070cf
Thorrester <sjforrester32@gmail.com>
2025-09-03 10:51:30 -0400

    not needed

--- crates/potato_prompt/src/lib.rs
+++ crates/potato_prompt/src/lib.rs
@@ -2,7 +2,7 @@ pub mod prompt;
 
 pub use prompt::{
     error::PromptError,
-    interface::{ModelSettings, Prompt},
+    interface::Prompt,
     types::{
         parse_response_to_json, AudioUrl, BinaryContent, DocumentUrl, ImageUrl, Message,
         PromptContent, Role, Score,
