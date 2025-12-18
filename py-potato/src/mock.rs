c88546eaa56bdd7f9740f8d997fe875f0407c489
Thorrester <sjforrester32@gmail.com>
2025-12-18 14:25:30 -0500

    fix import structure

--- py-potato/src/mock.rs
+++ py-potato/src/mock.rs
@@ -1,8 +1,7 @@
 use ::potato_head::LLMTestServer;
 use pyo3::prelude::*;
 
-#[pymodule]
-pub fn mock(m: &Bound<'_, PyModule>) -> PyResult<()> {
+pub fn add_mock_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<LLMTestServer>()?;
 
     Ok(())
