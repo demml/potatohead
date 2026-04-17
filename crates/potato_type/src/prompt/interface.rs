0e0af4678576b7f58aa2b1d0f53aa38225abba80
Thorrester <sjforrester32@gmail.com>
2026-04-17 10:44:11 -0400

    hide pyerr

--- crates/potato_type/src/prompt/interface.rs
+++ crates/potato_type/src/prompt/interface.rs
@@ -355,7 +355,7 @@ impl Prompt {
     }
 
     #[pyo3(signature = (path = None))]
-    pub fn save_prompt(&self, path: Option<PathBuf>) -> PyResult<PathBuf> {
+    pub fn save_prompt(&self, path: Option<PathBuf>) -> Result<PathBuf, TypeError> {
         let save_path = path.unwrap_or_else(|| PathBuf::from(SaveName::Prompt));
         PyHelperFuncs::save_to_json(self, &save_path)?;
         Ok(save_path)
--- crates/potato_type/src/prompt/mod.rs
+++ crates/potato_type/src/prompt/mod.rs
@@ -2,7 +2,6 @@ pub mod builder;
 pub mod interface;
 pub mod settings;
 pub mod types;
-
 pub use interface::*;
 pub use settings::*;
 pub use types::*;
