d4ead4805677af825809019b6d5b01ebe474b6d8
Thorrester <sjforrester32@gmail.com>
2026-04-18 08:50:20 -0400

    Update lib.rs

--- crates/potato_head/src/lib.rs
+++ crates/potato_head/src/lib.rs
@@ -1,23 +1,27 @@
-pub use potato_agent::*;
+pub mod prelude {
+    pub use potato_agent::*;
 
-pub use potato_type::{
-    anthropic as anthropic_types, error::TypeError, google as google_types, openai as openai_types,
-    prompt as prompt_types, Provider, SaveName, StructuredOutput,
-};
-pub use potato_util::{
-    calculate_weighted_score, create_uuid7, depythonize_object_to_value, error::UtilError,
-    utils::ResponseLogProbs, utils::TokenLogProbs, version, PyHelperFuncs,
-};
+    pub use potato_type::{
+        anthropic as anthropic_types, error::TypeError, google as google_types,
+        openai as openai_types, prompt as prompt_types, Provider, SaveName, StructuredOutput,
+    };
+    pub use potato_util::{
+        calculate_weighted_score, create_uuid7, depythonize_object_to_value, error::UtilError,
+        utils::ResponseLogProbs, utils::TokenLogProbs, version, PyHelperFuncs,
+    };
 
-pub use potato_provider::{
-    providers::embed::{EmbeddingInput, PyEmbedder},
-    ChatResponse, Embedder, EmbeddingConfig, EmbeddingResponse, GenAiClient, GoogleAuth,
-    OpenAIAuth,
-};
+    pub use potato_provider::{
+        providers::embed::{EmbeddingInput, PyEmbedder},
+        ChatResponse, Embedder, EmbeddingConfig, EmbeddingResponse, GenAiClient, GoogleAuth,
+        OpenAIAuth,
+    };
 
-pub use potato_workflow::*;
+    pub use potato_workflow::*;
 
-pub use potato_spec::{LoadedSpec, PotatoSpec, SpecError, SpecLoader};
+    pub use potato_spec::{LoadedSpec, PotatoSpec, SpecError, SpecLoader};
 
-#[cfg(feature = "mock")]
-pub use baked_potato::mock;
+    #[cfg(feature = "mock")]
+    pub use baked_potato::mock;
+}
+
+pub use prelude::*;
